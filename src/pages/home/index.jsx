e94d0c69c9ed370e82599a57dd916a7308edc7a4
JUCAGIO <ushaolinmatadordeporco@gmail.com>
2026-04-06 15:30:12 -0300

    footerzin

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,6 +11,12 @@ function Home() {
           Inventario Bento Quirino
         </h1>
       </header>
+      <div className="Body">
+        <p>conteúdo da página/app/site</p>
+      </div>
+      <footer className='Footer'>
+        <h5>Todos os direitos reservados para nós os sigmas - ETEC BQ 2026 - Devs: Hugi e JUCAGIO - coisas aleatorias pra encher o footer</h5>
+      </footer>
     </div>
   );
 }
