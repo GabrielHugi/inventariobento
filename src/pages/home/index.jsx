420f71e2b5001d3a34a49ecd866bf468384725f7
JUCAGIO <ushaolinmatadordeporco@gmail.com>
2026-04-09 13:53:03 -0300

    hugi bobao

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,27 +1,38 @@
-import logo from '../../images/logo.png';
-import './index.css';
-import '../shared/index.css'
+import logo from "../../images/logo.png";
+import "./index.css";
+import "../shared/index.css";
 
 function Home() {
   return (
     <div className="Home">
       <header className="Header">
-        <div style={{width: "20%", textAlign: "center"}}>
-        <img src={logo} alt="logo" className="Logo" />
+        <div style={{ width: "20%", textAlign: "center" }}>
+          <img src={logo} alt="logo" className="Logo" />
         </div>
-        <div style={{width: "60%", textAlign: "center"}}>
-        <h1>
-          Inventario Bento Quirino
-        </h1>
+        <div style={{ width: "60%", textAlign: "center" }}>
+          <h1>Inventario Bento Quirino</h1>
         </div>
-        <div style={{width: "20%"}}>
-        </div>
+        <div style={{ width: "20%" }}></div>
       </header>
       <div className="Body">
-        <p>conteúdo da página/app/site</p>
+        <div className="tela de login">
+          Email:
+          <form action="getform.php" method="get">
+            Nome: <input type="text" name="nome" />
+            <br />
+            Sobrenome: <input type="text" name="sobrenome" />
+            <br />
+            E-mail: <input type="email" name="email_usuario" />
+            <br />
+            <input type="submit" value="Enviar" />
+          </form>
+        </div>
       </div>
-      <footer className='Footer'>
-        <h5>Todos os direitos reservados para nós os sigmas - ETEC BQ 2026 - Devs: Hugi e JUCAGIO - coisas aleatorias pra encher o footer</h5>
+      <footer className="Footer">
+        <h5>
+          Todos os direitos reservados para nós os sigmas - ETEC BQ 2026 - Devs:
+          Hugi e JUCAGIO - coisas aleatorias pra encher o footer
+        </h5>
       </footer>
     </div>
   );
